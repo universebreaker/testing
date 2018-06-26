b99c62b74fd20456b9db3ac5ed33897ba3f0cdee
Walter Yung <walter.yung@paymentasia.com>
2018-06-26 14:13:48 +0900

    bug fix

--- part2_ptt_crawler.py
+++ part2_ptt_crawler.py
@@ -2,7 +2,7 @@ from PttWebCrawler.crawler import *
 import json
 
 # TODO: modify PttWebCrawler to add agent string(?), remove json file output
-def PTT_crawler(page_start = 1, page_end = 100, zone = 'PublicServan')
+def PTT_crawler(page_start = 1, page_end = 100, zone = 'PublicServan'):
     c = PttWebCrawler(as_lib=True)
     c.parse_articles(page_start, page_end, zone)
     with open('./'+zone + '-' + str(start) + '-' + str(end) + '.json','r') as fp:
