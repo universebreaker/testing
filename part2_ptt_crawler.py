cb5fb8ac9de5bd6da7ea7aafd4526d78ccd0e06f
Walter Yung <walter.yung@paymentasia.com>
2018-06-26 14:39:52 +0900

    json name fix

--- part2_ptt_crawler.py
+++ part2_ptt_crawler.py
@@ -5,7 +5,7 @@ import json
 def PTT_crawler(page_start = 1, page_end = 100, zone = 'PublicServan'):
     c = PttWebCrawler(as_lib=True)
     c.parse_articles(page_start, page_end, zone)
-    with open('./'+zone + '-' + str(start) + '-' + str(end) + '.json','r') as fp:
+    with open('./'+zone + '-' + str(page_start) + '-' + str(page_end) + '.json','r') as fp:
         buf = wp.read()
     load = json.loads(buf)
     for article in load['articles']:
